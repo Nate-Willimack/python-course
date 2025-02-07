d20fe697e4de023ea799cd412960c9e90b2a8e4d
Nate_Willimack <natewillimack@gmail.com>
2025-02-07 11:58:21 -0600

    made pdf merger using for loop and merge using pypdf2

--- python.py
+++ python.py
@@ -1,1 +1,10 @@
-print("hello world!")
+import PyPDF2
+import os
+
+merge = PyPDF2.PdfMerger()
+
+for file in os.listdir(os.getcwd()):
+    if file.endswith(".pdf"):
+        merge.append(file)
+
+merge.write("combinedPDF.pdf")
